faa750a7891264fdb381c81dac4fa4db93baa8c5
David <davids@codeprecise.com>
2017-12-15 21:15:48 +0200

    1

--- Arenstof.cpp
+++ Arenstof.cpp
@@ -1,5 +1,23 @@
 #include "Arenstof.h"
 
+Arenstof::Arenstof(
+    const ArenstofPoint& initialPoint,
+    long double deltaT,
+    int m,
+    std::ostream& out)
+:
+    _point(initialPoint),
+    _deltaT(deltaT),
+    _m(m),
+    _out(out)
+{
+}
+
+void Arenstof::Update()
+{
+    // calculate the next step and write to the output if required
+}
+
 int Arenstof::computeArenstof(
     const ArenstofPoint& init,
     unsigned int n,
