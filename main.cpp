2f470f1b5a4720d4f326295b0dcb99d433046a7c
Danielle <danielle.sackstein94@gmail.com>
2017-12-15 22:12:12 +0200

    2

--- main.cpp
+++ main.cpp
@@ -1,29 +1,28 @@
 #include <iostream>
 #include "Arenstof.h"
 
-int main() {
+int main()
+{
 
-    long double x = 1;
-    long double y = 1;
-    long double vx = 1;
-    long double vy = 1;
-    long double T = 100;
-    int n = 8;
-    int m = 2;
+	long double x = 1;
+	long double y = 1;
+	long double vx = 1;
+	long double vy = 1;
+	long double T = 100;
+	int n = 8;
+	int m = 2;
 
-    long double deltaT = T / m;
+	long double deltaT = T / m;
 
-    Arenstof arenstof(
-        ArenstofPoint(x, y, vx, vy),
-        deltaT, m);
+	Arenstof arenstof(ArenstofPoint(x, y, vx, vy), deltaT, m);
 
-    for (int i=0; i<m; i++)
-    {
-        arenstof.Update();
-    }
+	for (int i = 0; i < m; i++)
+	{
+		arenstof.Update();
+	}
 
-    ArenstofPoint p (1, 2, 3, 4);
-    std::cout << p << "\n";
+	ArenstofPoint p(1, 2, 3, 4);
+	std::cout << p << "\n";
 
-    return 0;
+	return 0;
 }
