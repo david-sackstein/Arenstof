02aeecacea870636f32002e532d7c2fa176ded3f
Danielle <danielle.sackstein94@gmail.com>
2017-12-15 22:11:33 +0200

    2

--- Arenstof.h
+++ Arenstof.h
@@ -5,10 +5,30 @@
 
 class Arenstof
 {
-    static int computeArenstof(
-        const ArenstofPoint& init,
-        unsigned int n,
-        unsigned int m,
-        long double dt,
-        std::ostream& out);
+public:
+	Arenstof(
+			const ArenstofPoint &initialPoint,
+			long double deltaT,
+			int m,
+			std::ostream &out);
+
+	Arenstof(ArenstofPoint point, long double d, int i);
+
+	void Update();
+
+
+	static int computeArenstof(
+			const ArenstofPoint &init,
+			unsigned int n,
+			unsigned int m,
+			long double dt,
+			std::ostream &out);
+
+private:
+	ArenstofPoint _point;
+	long double _deltaT;
+	int _m;
+	std::ostream &_out;
+
+
 };
