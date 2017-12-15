c0ab9fed0b4cc41934c4709481d42e212ecfbab9
David <davids@codeprecise.com>
2017-12-15 21:22:15 +0200

    1

--- main.cpp
+++ main.cpp
@@ -3,6 +3,25 @@
 
 int main() {
 
+    long double x = 1;
+    long double y = 1;
+    long double vx = 1;
+    long double vy = 1;
+    long double T = 100;
+    int n = 8;
+    int m = 2;
+
+    long double deltaT = T / m;
+
+    Arenstof arenstof(
+        ArenstofPoint(x, y, vx, vy),
+        deltaT, m);
+
+    for (int i=0; i<m; i++)
+    {
+        arenstof.Update();
+    }
+
     ArenstofPoint p (1, 2, 3, 4);
     std::cout << p << "\n";
 
